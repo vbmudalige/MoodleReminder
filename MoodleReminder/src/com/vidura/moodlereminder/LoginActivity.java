58a4313e8a01a73fad9eb0bfa7d9e8dfebe5b9c0
vidurab <vbmudalige@gmail.com>
2014-07-06 20:03:37 +0530

    write the code to activate the login button

--- MoodleReminder/src/com/vidura/moodlereminder/LoginActivity.java
+++ MoodleReminder/src/com/vidura/moodlereminder/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.vidura.moodlereminder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -11,5 +15,17 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		Button b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(new OnClickListener()
+				
+				{
+
+					@Override
+					public void onClick(View v) {
+						startActivity(new Intent(LoginActivity.this, WelcomeActivity.class));
+					}
+				
+				});
 	}
 }
